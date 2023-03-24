38b9d3c5050e85fc241a6a89a13164fd3e5765da
Ronkl14 <121381849+Ronkl14@users.noreply.github.com>
2023-03-24 19:27:04 +0300

    added cash update

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -18,3 +18,47 @@ export const createNewAccount = asyncHandler(async (req, res, next) => {
     data: account,
   });
 });
+
+//@desc     Update cash value of an account
+//@route    PUT /api/v1/accounts/:id
+//@access   Private
+export const updateCash = asyncHandler(async (req, res, next) => {
+  const id = req.params.id;
+
+  if (req.body.deposit) {
+    const account = await Account.findByIdAndUpdate(
+      id,
+      {
+        $inc: { cash: req.body.deposit },
+      },
+      { new: true }
+    );
+
+    res.status(200).json({
+      success: true,
+      data: account,
+    });
+  }
+
+  if (req.body.withdraw) {
+    const withdraw = req.body.withdraw;
+    let account = await Account.findById(id);
+
+    if (account.cash - withdraw < account.credit * -1) {
+      res.status(400).json({
+        success: false,
+        message: "Not enough credit",
+      });
+    } else {
+      account = await Account.findByIdAndUpdate(
+        id,
+        { $inc: { cash: -withdraw } },
+        { new: true }
+      );
+      res.status(200).json({
+        success: true,
+        data: account,
+      });
+    }
+  }
+});
--- routes/router.js
+++ routes/router.js
@@ -1,11 +1,16 @@
 import express from "express";
 import { createNewUser } from "../controllers/userController.js";
-import { createNewAccount } from "../controllers/accountController.js";
+import {
+  createNewAccount,
+  updateCash,
+} from "../controllers/accountController.js";
 
 const router = express.Router();
 
 router.route("/users").post(createNewUser);
 
 router.route("/accounts").post(createNewAccount);
+
+router.route("/accounts/:id").put(updateCash);
 
 export default router;
